00cbbe0aab32dcfad0360bafbec52c568b33207f
Hector Daniel Espinosa Moreno <hdaniel.espinosa@gmail.com>
2021-09-14 01:10:59 -0500

    Creación de metodos y controllers para los métodos create, update y delete del módulo clientes

--- src/main/java/com/hdaniel/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/hdaniel/springboot/backend/apirest/models/services/IClienteService.java
@@ -7,5 +7,11 @@ import com.hdaniel.springboot.backend.apirest.model.entity.Cliente;
 public interface IClienteService {
 	
 	public List<Cliente> findAll();
+	
+	public Cliente findById(Long id);
+	
+	public Cliente save(Cliente cliente);
+	
+	public void delete(Long id);
 
 }
